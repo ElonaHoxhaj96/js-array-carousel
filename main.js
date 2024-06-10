8121c208cfdf1e5cbc30ff212cd40d350aa3bdeb
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-10 16:07:57 +0200

    feat: added in html the icone from font awesome, added some style in css

--- main.js
+++ main.js
@@ -7,4 +7,27 @@ const immaginiArray = [
     'img/05.webp'
 ];
 
-console.log(immaginiArray)
+//estrapoliamo l'elemnto del contenitore di HTML
+const imgCarosello = document.querySelector("#container");
+
+//estrapoliamo gli elementi dall array
+for(let i = 0; i < immaginiArray.length; i++){
+    let imgFile = immaginiArray[i];
+    // console.log(imgFile);
+    // creare l'elemento da inserire ad ogni interazione
+    let itemCarosello = `
+                <div class="photo_img > 
+                    <img src= "${imgFile}">
+                </div>`;
+    //inseriamo questo nuovo elemento al contenitore
+    imgCarosello.innerHTML +=  itemCarosello    
+}
+
+//selezioniamo tutti gli elementi photo_img della pagina
+const photo_img = document.getElementsByClassName("photo_img");
+// console.log(photo_img)
+
+// rendiamo visibile il primo elemento 
+photo_img[0].classList.add("visible");
+console.log(photo_img[0]);
+
