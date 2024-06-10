d579439855f80f25f63a1b6d64c321e09377948a
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-10 14:52:55 +0200

    feat: creating Array with the images in JS file

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // creiamo una Array che contenga le mie immagini
 const immaginiArray = [
-    
-]
+    'img/04.webp',
+    'img/03.webp',
+    'img/01.webp',
+    'img/02.webp',
+    'img/05.webp'
+];
+
+console.log(immaginiArray)
